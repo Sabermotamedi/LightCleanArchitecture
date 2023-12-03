a2596f7f602d74202a21ba119488277f7e596421
Saber Motamedi <saber.motamedi@totallytech.co>
2023-12-04 01:51:30 +0400

    Initial commit Set up project structure and files, including basic configuration files.

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
+
---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        return services;
+    }
+}
+
